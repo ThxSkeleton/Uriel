60d588d14bbf757c61e61a4dca4c51b214ea4054
ThanksSkeleton <thanksSkeleton@gmail.com>
2018-11-04 20:19:13 -0800

    Missing Files

--- OpenGL.Net.Objects.Test/GraphicsContext.cs
+++ OpenGL.Net.Objects.Test/GraphicsContext.cs
@@ -30,7 +30,7 @@ namespace OpenGL.Objects.Test
 	[TestFixture]
 	class GraphicsContextTest
 	{
-		[Test(Description = "Test GraphicsContext(DeviceContext, IntPtr)")]
+		[Test, Category("Objects")]
 		public void TestConstructorRaw()
 		{
 			using (DeviceContext deviceContext = DeviceContext.Create()) {
@@ -60,7 +60,7 @@ namespace OpenGL.Objects.Test
 			}
 		}
 
-		[Test(Description = "Test GraphicsContext(DeviceContext, GraphicsContext, KhronosVersion, GraphicsContextFlags)")]
+		[Test, Category("Objects")]
 		public void TestConstructor()
 		{
 			using (DeviceContext deviceContext = DeviceContext.Create()) {
@@ -76,7 +76,7 @@ namespace OpenGL.Objects.Test
 		private void TestConstructProperties(GraphicsContext graphicsContext)
 		{
 			if (graphicsContext == null)
-				throw new ArgumentNullException("graphicsContext");
+				throw new ArgumentNullException(nameof(graphicsContext));
 
 			Assert.IsTrue(graphicsContext.IsCurrent, "not current");
 			Assert.IsNotNull(graphicsContext.Version, "Version is null");
@@ -89,7 +89,7 @@ namespace OpenGL.Objects.Test
 		private void TestDisposedProperties(GraphicsContext graphicsContext)
 		{
 			if (graphicsContext == null)
-				throw new ArgumentNullException("graphicsContext");
+				throw new ArgumentNullException(nameof(graphicsContext));
 
 			Assert.AreEqual(IntPtr.Zero, graphicsContext.Handle, "GL context handle is not zero");
 			Assert.AreEqual(Guid.Empty, graphicsContext.ObjectNameSpace, "object namespace still defined");
