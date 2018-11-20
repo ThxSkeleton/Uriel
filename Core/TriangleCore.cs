1048c4c3f75de0a5eb031f0ba1d290a23fece398
ThanksSkeleton <thanksSkeleton@gmail.com>
2018-11-19 20:32:36 -0800

    remove usings

--- Core/TriangleCore.cs
+++ Core/TriangleCore.cs
@@ -1,10 +1,6 @@
 ﻿using Uriel.DataTypes;
 using OpenGL;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Uriel
 {
